49f7ec7bef0b6ec565dbc1b27e6be17cb1e965aa
wangxiaoke123 <42530266+wangxiaoke123@users.noreply.github.com>
2018-09-03 14:26:05 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -2,8 +2,8 @@
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyARHJJDFtwb5wcnURwwJA3FQRrM16PF27c');
 define('GJ_CODE', 'JP');
-define('SITE_NAME', '大爷常来玩儿啊');
-define('TITLENAME', 'DouDouTube');
+define('SITE_NAME', '油管');
+define('TITLENAME', 'XiaokeTube');
 define('EN2DEKEY', 'lujianbupingyishenghou');
-define('EMAIL', 'xiaofufua@protonmail.com');
+define('EMAIL', 'chivbnm111111@gmail.com');
 ?>
